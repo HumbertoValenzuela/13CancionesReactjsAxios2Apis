335aba4a768c45b7d32dcbe8394a66cb0443508f
hrvg <hrvg.computacion@gmail.com>
2021-09-13 01:31:27 -0300

    13 canciones reactjs axios2apis bootstrap useState useEffects props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import Info from './components/Info';
+
+
+// https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search
+
 
 function App() {
+
+  // state para la busqueda de la letra
+  const [ busquedaLetra, guardarBusquedaLetra] = useState( {} );
+
+  // Agregar state para la letra
+  const [letra, guardarLetra] = useState('');
+
+  // Agregar state de info de la api
+  const [info, guardarInfo] = useState( {} );
+
+  const [ error, guardarError] = useState(false);
+
+  useEffect(() => {
+    // La primera vez busqueda letra esta vacio
+    if (Object.keys( busquedaLetra).length === 0 ) return;
+    // if (Object.keys( guardarInfo).length === 0 ) return;
+    // console.log('no se ejecuta');
+
+    
+    const consultarApiLetra = async () => {
+
+      try {
+        
+        const { artista, cancion } = busquedaLetra;
+  
+        const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+  
+        const url2= `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+        const [ letra, informacion ] = await Promise.all([
+          axios.get(url),
+          axios.get(url2)
+        ])
+          .catch(
+            (error) => {
+              guardarError( true );
+            }
+          );
+
+          guardarError( false );
+
+        // console.log(letra);
+        // console.log(letra.data.lyrics);
+        // console.log(informacion);
+        // console.log(informacion.data.artists[0]);
+  
+        //  console.log(resultado);// info de la API.
+        // console.log(resultado.data.lyrics); // para acceder a la letra de la canción
+        
+        // guardarLetra( resultado.data.lyrics );
+
+        guardarLetra( letra.data.lyrics );
+        
+        guardarInfo( informacion.data.artists[0])
+
+      } catch (error) {
+
+        console.log(error);
+      }
+    }
+
+    consultarApiLetra();
+
+  }, [ busquedaLetra, info ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario 
+        guardarBusquedaLetra= { guardarBusquedaLetra }  
+      />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            {
+              (error)
+                ? 
+                <p className="alert alert-primary text-center">
+                No se encontró la Información
+                </p>
+                :
+                <Info info= { info } />
+            }
+          </div>
+
+          <div className="col-md-6">
+          {
+              (error)
+                ? 
+                <p className="alert alert-primary text-center">
+                No se encontró la letra de la canción
+                </p>
+                :
+                <Cancion letra= { letra } />
+            }
+            
+          </div>
+
+        </div>
+
+      </div>
+    </Fragment>
   );
 }
 
